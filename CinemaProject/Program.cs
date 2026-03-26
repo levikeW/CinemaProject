dcb36f5b3483bf5ff468a585c894125ac1db79e1
NuszbaumHeni <nuszbaum.henriett20k@egressy.info>
2026-03-26 12:37:31 +0100

    addmigration

--- CinemaProject/Program.cs
+++ CinemaProject/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
         {
             // when using credentials (cookies/auth) the origin cannot be '*' – must specify exact hosts
             policy
-                .WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .WithOrigins("http://127.0.0.1:5501", "http://localhost:5501")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
