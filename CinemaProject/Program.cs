b42b1790074207251b1b02e6313433bb545e1d75
NuszbaumHeni <nuszbaum.henriett20k@egressy.info>
2026-04-26 17:07:59 +0200

    delete weather.cs and controller

--- CinemaProject/Program.cs
+++ CinemaProject/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddTransient<CartModel>();
 builder.Services.AddTransient<Payment_ReservationModel>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
